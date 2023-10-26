59b4f07a80546e2f35d327652932e09790021bae
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2023-10-27 00:12:57 +0530

    slight improvement in ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,16 +12,16 @@ export default function Home() {
             <div className='font-bold text-2xl '>OUR SERVICES</div>
             <div className='flex items-center justify-evenly w-screen'>
 
-              <div className='flex flex-col border border-white w-40 h-40'>
+              <div className='flex items-center justify-center flex-col font-bold border border-white w-40 h-40'>
                 MANAGE EMPLOYEE
               </div>
-              <div className='flex flex-col border border-white w-40 h-40'>
+              <div className='flex items-center justify-center flex-col font-bold border border-white w-40 h-40'>
                 MANAGE DEPARTMENT
               </div>
-              <div className='flex flex-col border border-white w-40 h-40'>
+              <div className='flex items-center justify-center flex-col border font-bold border-white w-40 h-40'>
                 HANDLE PROMOTIONS
               </div>
-              <div className='flex flex-col border border-white w-40 h-40'>
+              <div className='flex flex-col items-center justify-center border font-bold border-white w-40 h-40'>
                 ASSIGN EMPLOYEE
               </div>
 
